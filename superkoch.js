4f186a19ea5600832b9bc8eabefbfdd1ff89f915
m1de-d3c <clclanhellohello@gmail.com>
2023-06-18 20:04:45 -0400

    First commit.

---
+++ superkoch.js
@@ -0,0 +1,14 @@
+const http  = require("http");
+const fs    = require("fs");
+
+const host = "localhost";
+const port = 8000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200);
+    var http = fs.readFileSync("./server/index.html").toString("utf-8");
+    res.end(http);
+});
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
